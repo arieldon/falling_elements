3e03e78a0cc21f4f80978200943f3602e2437400
Ariel Don <ariel@arieldon.com>
2023-09-16 12:46:14 -0500

    Explain procedure to draw brush in more detail

--- main.c
+++ main.c
@@ -59,42 +59,92 @@ SetBrushPixel(s32 X, s32 Y)
 static void
 DrawBrush(void)
 {
+	// NOTE(ariel) I stole this code from Casey Muratori's Computer Enhance
+	// lecture [0] where he explains his implementation of a DDA (Digital
+	// Differential Analyzer) circle outline algorithm.
+	//
+	// It's far from the most efficient method to draw a circle on modern
+	// hardware, especially my use of SetBrushPixel() below, but it's super
+	// simple. It doesn't even use multiplication explicitly! Plus, I don't need
+	// to link and include `math.h` this way or implement sin() and cos() with
+	// polynomial approximations or some other method.
+	//
+	// [0] https://www.computerenhance.com/p/efficient-dda-circle-outlines
 	if (!MenuContext.MenuIsHot && Input.CursorIsInWindow)
 	{
-		int Cx = Input.MousePositionX;
-		int Cy = Input.MousePositionY;
-		int R = 32;
+		s32 CenterX = Input.MousePositionX;
+		s32 CenterY = Input.MousePositionY;
 
-		// NOTE(casey): Loop that draws the circle
+		s32 Radius = 32;
+		s32 Diameter = Radius + Radius;
+
+		// NOTE(ariel) Start drawing circle's outline from x-axis in first
+		// quadrant: x = (Radius, 0).
+		//       |
+		//       |
+		// ------+----x-- Let Radius = 5 in this case.
+		//       |
+		//       |
+		s32 X = Radius;
+		s32 Y = 0;
+
+		s32 dY = -2;
+		s32 dX = Diameter+Diameter - 4;
+		s32 D = Diameter - 1;
+
+		// NOTE(ariel) Use a signed distance field specified by the implicit
+		// definition of a circle f(x, y) = x*x + y*y - r*r = 0, though we don't
+		// use this formula directly because we simplify it further for this
+		// specific case. An implicit definition serves this problem well because
+		// the plotting _starts_ on the circle at (Radius, 0) by default. With each
+		// iteration of the loop, the algorithm then chooses which pixel to plot
+		// next.
+		//
+		// We essentially choose between two points A and B in each iteration,
+		// where A=(Radius-1, Y+1) and B=(Radius, Y+1). We choose the point closer
+		// to the center of the circle, i.e. |f(A)| < |f(B)| choose A, otherwise B.
+		//
+		// If we plug A and B into the implicit circle equation f above and push
+		// all the variables onto one side to compare against zero, we find this
+		// equation: d(x, y) = -2x*x + 2x - 2y*y + c ?< 0, where c=2r*r - 1.
+		//
+		// Next we use a digital differential analyzer to find the following
+		// equation: e(x, y) = d(x, y+1) - d(x, y) = -4y - 2. This equation
+		// indicates change per step -- they're derivatives, and it's where the
+		// terms for `dY` originate. We then apply the same technique again as
+		// follows: e(x, y+1) - e(x, y) = -4.
+		//
+		// We follow the same procedure for d(x-1, y) - d(x, y) to find the change
+		// in `X` each iteration of the loop.
+		while (Y <= X)
 		{
-			int R2 = R+R;
+			// NOTE(ariel) Exploit the circle's perfect symmetry to plot all octant
+			// whilst only explicitly computing coordinates for the first, where the
+			// first octant is the half of the first quadrant closer to the x-axis.
+			// This allows us to increment `Y` in each iteration of the loop since we
+			// stop before the relationship between X and Y essentially flips,
+			// becoming A=(X-1,Radius-1) and B=(X-1, Radius).
+			SetBrushPixel(CenterX - X, CenterY - Y);
+			SetBrushPixel(CenterX + X, CenterY - Y);
+			SetBrushPixel(CenterX - X, CenterY + Y);
+			SetBrushPixel(CenterX + X, CenterY + Y);
+			SetBrushPixel(CenterX - Y, CenterY - X);
+			SetBrushPixel(CenterX + Y, CenterY - X);
+			SetBrushPixel(CenterX - Y, CenterY + X);
+			SetBrushPixel(CenterX + Y, CenterY + X);
 
-			int X = R;
-			int Y = 0;
-			int dY = -2;
-			int dX = R2+R2 - 4;
-			int D = R2 - 1;
+			// NOTE(ariel) Account for motion along y-axis.
+			D += dY;
+			dY -= 4;
+			Y += 1;
 
-			while(Y <= X)
-			{
-				SetBrushPixel(Cx - X, Cy - Y);
-				SetBrushPixel(Cx + X, Cy - Y);
-				SetBrushPixel(Cx - X, Cy + Y);
-				SetBrushPixel(Cx + X, Cy + Y);
-				SetBrushPixel(Cx - Y, Cy - X);
-				SetBrushPixel(Cx + Y, Cy - X);
-				SetBrushPixel(Cx - Y, Cy + X);
-				SetBrushPixel(Cx + Y, Cy + X);
-
-				D += dY;
-				dY -= 4;
-				++Y;
-
-				int Mask = (D >> 31);
-				D += dX & Mask;
-				dX -= 4 & Mask;
-				X += Mask;
-			}
+			// NOTE(ariel) Account for motion along x-axis. Shift D right by 31
+			// bits to use its sign bit essentially as a boolean, adjusting values
+			// along x if and only if D is less than zero.
+			b32 LessThanZeroMask = D >> 31;
+			D += dX & LessThanZeroMask;
+			dX -= 4 & LessThanZeroMask;
+			X += LessThanZeroMask;
 		}
 	}
 }
